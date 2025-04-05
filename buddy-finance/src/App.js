093ef1a11ed6cb557bd86571a0ca601c5fdb9561
Muneendra <muneendravipparthi@gmail.com>
2025-04-05 11:58:22 +0800

    new changes

--- buddy-finance/src/App.js
+++ buddy-finance/src/App.js
@@ -1,49 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import RegisterPage from "./pages/RegisterPage.js";
-// import LoginPage from "./pages/LoginPage.js";
-import HomePage from "./pages/HomePage.js";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
-      <Routes>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Route path="/home" element={<HomePage />} />
-        {/* <Route path="/register" element={<RegisterPage />} /> */}
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-        </header>
-        </div>
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
